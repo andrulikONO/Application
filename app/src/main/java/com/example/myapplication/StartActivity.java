f5c240db2cfecbc624d2033244e92aacad1e1838
rpik <>
2025-06-27 17:12:48 +0300

    обработка нажатия кнопки на начальном экране

--- app/src/main/java/com/example/myapplication/StartActivity.java
+++ app/src/main/java/com/example/myapplication/StartActivity.java
@@ -1,12 +1,24 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.button.MaterialButton;
 
 public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        MaterialButton startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, StartDialogActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 } 
